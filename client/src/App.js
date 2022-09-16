d3f3a33aa2ba8e0ee20c78096c0016a0d6101b14
Alberto Morales <swoord.sla@gmail.com>
2022-09-16 14:23:15 -0500

    feat: Initialize project using Create React App

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-title"> ¡Hello World! </p>
+        <p> Lorem Ipsum... </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
