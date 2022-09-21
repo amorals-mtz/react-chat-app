7359e3f6c7397b6374ce3f3d707b8417d980462c
Alberto Morales <swoord.sla@gmail.com>
2022-09-20 21:39:30 -0500

    feat(client): Add home page for the chat application.

---
+++ client/src/components/Home.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+/** 
+ * Home page for the chat application that accepts the username 
+ * and saves it to the local storage for identification.
+ */
+const Home = () => {
+    const navigate = useNavigate();
+    const [ userName, setUserName ] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.setItem('userName', userName);
+        navigate('/chat');
+    };
+    return (
+        <form className="home__container" onSubmit={handleSubmit}>
+            <h2 className="home__header"> Sign in to Open Chat </h2>
+            <label htmlFor="username"> Username </label>
+            <input
+                id="username"
+                type="text"
+                className="username__input"
+                minLength={6}
+                name="username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+            />
+            <button className="home__cta"> SIGN IN </button>
+        </form>
+    );
+};
+
+export default Home;
